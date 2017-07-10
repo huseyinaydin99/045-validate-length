835b96b1895bda69f059b84d25483ca6ade87690
huso51 <huseyinaydin99@gmail.com>
2017-07-11 00:41:11 +0300

    Add files via upload

---
+++ 045_ValidateLength/src/java/com/siteadi/SonucManagedBean.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.siteadi;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author husey
+ */
+@ManagedBean
+@RequestScoped
+public class SonucManagedBean {
+
+    /**
+     * Creates a new instance of SonucManagedBean
+     */
+    public SonucManagedBean() {
+    }
+    
+    String cumle;
+
+    public String getCumle() {
+        return cumle;
+    }
+
+    public void setCumle(String cumle) {
+        this.cumle = cumle;
+    }
+    
+    
+}
